8306bb53c59c28d87e909ea6dcf3715fcb13f4fd
Keketso <kekekeketso03@gmail.com>
2024-12-03 11:00:59 +0200

    completed

--- src/Pages/TopicsCovered.jsx
+++ src/Pages/TopicsCovered.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import projectList from "../Store/data";
 
 function TopicsCovered() {
   return (
-    <div>TopicsCovered</div>
-  )
+    <div className="container p-5 text-white">
+      <div className="h3 text-success">Topics Covered</div>
+      <hr />
+      <div className="row">
+        {projectList.map((product, index) => (
+          <div key={index} className="border rounded m-3 p-2">
+            <div className="h5 text-center">{product.title} </div>
+            <p>{product.describe}</p>
+            <ul>
+              {product.topicsCovered.map((topics, index) => (
+                <li key={index}>{topics}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default TopicsCovered
+export default TopicsCovered;
